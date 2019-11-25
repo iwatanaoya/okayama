43bf42f11beada05ceb3f7678ebe390c95c6540f
iwatanaoya <55228775+iwatanaoya@users.noreply.github.com>
2019-11-25 15:55:01 +0900

    Create main.js

---
+++ main.js
@@ -0,0 +1,13 @@
+(function(){
+  "use strict";
+
+  var show=document.getElementById("show")
+  var hide=document.getElementById("hide")
+
+  show.addEventListener("click",function(){
+    document.body.className="menu-open";
+  });
+  hide.addEventListener("click",function(){
+    document.body.className="";
+  });
+})();
